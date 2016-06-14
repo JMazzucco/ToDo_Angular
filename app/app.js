50e85a91a064ec8c0c06a10d480efc763ad5483d
John Mazzucco <John.Mazzucco@gmail.com>
2016-06-14 19:04:47 -0400

    Order tasks by priority value

--- app/app.js
+++ app/app.js
@@ -4,9 +4,9 @@
 	app.controller('ToDoController', function($scope){
 
 		$scope.todoTasks = [
-			{text: 'Learn Angular', priority: 'high', done: false},
-			{text: 'Learn Python', priority: 'low', done: false},
-			{text: 'Train BJJ', priority: 'medium', done: false}
+			{text: 'Learn Angular', priority: '1', done: false},
+			{text: 'Learn Python', priority: '3', done: false},
+			{text: 'Train BJJ', priority: '2', done: false}
 		];
 
 		$scope.totalTodos = $scope.todoTasks.length;
